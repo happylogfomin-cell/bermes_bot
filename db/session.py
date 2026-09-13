74b4f903ed8931ddea27dbae7e5c81dd636b5897
happylogfomin-cell <happylogfomin@gmail.com>
2026-09-13 12:11:51 +0300

    Update session.py

--- db/session.py
+++ db/session.py
@@ -1,9 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from db.models import Base
-from config import load_config
 
-cfg = load_config()
-engine = create_async_engine(cfg.db_url, echo=False)
+DATABASE_URL = "sqlite+aiosqlite:///berm.db"
+
+engine = create_async_engine(DATABASE_URL, echo=False)
 SessionLocal = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 async def init_db():
